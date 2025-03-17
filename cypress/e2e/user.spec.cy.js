88eb5f83507aa566ce7b2ae788b4ba76553f25c5
Silvio Cesar <scff096@gmail.com>
2025-03-17 16:04:07 -0300

    My Info user update scenario Test Added

---
+++ cypress/e2e/user.spec.cy.js
@@ -0,0 +1,51 @@
+import userData from '../fixtures/users/user-data.json'
+
+describe('OrangeHRM Login Test', () => {
+
+    const selectorsList = {
+      usernameField: "[name='username']",
+      passwordField: "[name='password']",
+      loginButton: "[type='submit']",
+      sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+      dashboardGrid: ".orangehrm-dashboard-grid",
+      wrongCredentialAlert: "[role='alert']",
+      myInfoButton: "[href='/web/index.php/pim/viewMyDetails'] .oxd-text--span",
+      firstNameField: "[name='firstName']",
+      lastNameField: "[name='lastName']",
+      genericField: ".oxd-input--active",
+      dateField: "[placeholder='yyyy-dd-mm']",
+      dateCloseButton: ".--close",
+      saveButton: "[type='submit']",
+
+    }
+
+  it.only('User Info Update - Success', () => {
+
+    cy.visit('/auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
+    cy.get(selectorsList.loginButton).click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
+    cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
+    cy.get(selectorsList.genericField).eq(3).clear().type('EmplooyeId')
+    cy.get(selectorsList.genericField).eq(4).clear().type('OtherIdTest')
+    cy.get(selectorsList.genericField).eq(5).clear().type('DriversLicenseTest')
+    cy.get(selectorsList.genericField).eq(6).clear().type('2026-03-17')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.saveButton).eq(0).click()
+    cy.get('.oxd-toast-close')
+
+    
+  })
+  it('Login - Fail', () => {
+
+    cy.visit('/auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userFail.username)
+    cy.get(selectorsList.passwordField).type(userData.userFail.password)
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredentialAlert)
+  })
+})
